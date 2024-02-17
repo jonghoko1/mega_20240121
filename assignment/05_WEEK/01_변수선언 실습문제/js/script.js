ea5f796778aee90e2892cd93e38b4c8683e7aa51
RCN_98th_no21 <jonghoko3@naver.com>
2024-02-17 20:52:38 +0900

    5주차 토요일 숙제

---
+++ assignment/05_WEEK/01_변수선언 실습문제/js/script.js
@@ -0,0 +1,23 @@
+var userName;
+userName = "고종호";
+document.write("안녕하세요. 만나서 반갑습니다. 저는 " + userName + " 입니다.", "<br /><br />");
+console.log(userName);
+
+var thisYear;
+thisYear = 2024;
+document.write("올해의 연도는 " + thisYear + "년 입니다." , "<br /><br />");
+console.log(thisYear);
+
+var snoopyImage1;
+snoopyImage1 = "<img src='./img/snoopy_01.png'>";
+document.write(snoopyImage1, "<br /><br />");
+console.log(snoopyImage1);
+
+// var snoopyImage2;
+// snoopyImage2 = "<div class='snoopy' style='background-image: url(./img/snoopy_02.png);'></div>";
+// document.write(snoopyImage2, "<br /><br />");
+// console.log(snoopyImage2);
+var snoopyImage2;
+snoopyImage2 = "<div class='snoopy'></div>";
+document.write(snoopyImage2, "<br /><br />");
+console.log(snoopyImage2);
